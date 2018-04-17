ae4c22e0b3c1f9cf255677315dbf5b58bfdceef4
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-17 18:13:25 +0900

    add static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_page/index'
+  get 'static_page/about'
+  get 'static_page/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
