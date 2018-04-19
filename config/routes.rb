518c5dab9bb982e19a1c35e1513f69e78ddb8a58
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-19 13:12:16 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_page#index'
   resources :posts
-  get 'static_page/index'
   get 'static_page/about'
   get 'static_page/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
