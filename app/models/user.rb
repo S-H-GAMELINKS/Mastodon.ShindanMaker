97bc04342b043ad831c25bf3447809579535c842
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-19 17:40:59 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
       
     unless user
       user = User.create(
-        name:     auth.info.extra.raw_info.display_name,
+        name:     auth.info.extra.display_name,
         uid:      auth.uid,
         provider: auth.provider,
         email:    User.dummy_email(auth),
