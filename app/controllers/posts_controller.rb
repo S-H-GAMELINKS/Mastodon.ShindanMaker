f77b159784f9096bd814c4f3deb3fe5a3113cbc0
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-19 18:18:15 +0900

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,8 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
+
+        redirect_to root
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
