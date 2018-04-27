c70e9ee1d9e78af9e6071e8c5b6fc21d35ae42fc
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-27 17:34:07 +0900

    fix share to mastodon

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -13,5 +13,6 @@ class StaticPageController < ApplicationController
     @user = current_user.name
     @theme = Post.find(params[:id]).theme.split(" ")
     @title = "#" + Post.find(params[:id]).title
+    @result = "#{@user}さんは#{@theme[rand(@theme.count)]}#{@title}" + "\n #Mastodon診断アプリ"
   end
 end
